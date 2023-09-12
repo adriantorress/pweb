3d2fa1a57e82f53aa9b12f48108f59153fa3b328
Adrian <adriantorrescv@gmail.com>
2023-09-12 06:26:44 -0300

    Finish application

--- src/main/java/projetoweb/SchoolModel.java
+++ src/main/java/projetoweb/SchoolModel.java
@@ -1,8 +1,6 @@
 package projetoweb;
+
 import java.time.LocalDate;
-
-
-import org.springframework.format.annotation.DateTimeFormat;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -23,16 +21,18 @@ public class SchoolModel {
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Integer id;
 
-  @NotBlank(message = "Campo Obrigatório")
-  @Size(min =3, max = 50)
+  @NotBlank(message = "O campo não pode estar em branco")
   private String nome;
 
-  public LocalDate data_nascimento;
+  @NotNull(message = "A data não pode estar em branco")
+  @Past(message = "A data deve estar no passado")
+  public LocalDate dataNascimento;
 
- 
-  private String nomedamae;
+  @NotBlank(message = "O campo não pode estar em branco")
+  private String nomeDaMae;
 
-  private String nomedopai;
+  @NotBlank(message = "O campo não pode estar em branco")
+  private String nomeDoPai;
 
   @NotNull
   private Integer ddd;
@@ -43,22 +43,23 @@ public class SchoolModel {
   @NotNull
   private Integer ramal;
 
-  @NotEmpty
-  @Email
+  @NotEmpty(message = "O campo não pode estar em branco")
+  @Email(message = "Informe um email válido.")
   private String email;
 
+  @NotEmpty(message = "O campo não pode estar em branco.")
   private String serie;
 
-  @NotBlank
+  @NotBlank(message = "O campo não pode estar em branco.")
   private String turno;
 
-  @Size(max = 3)
+  @Size(max = 3, message = "Selecione no máximo 3 atividades.")
   public String[] atividades;
 
-  
   public Integer getId() {
     return id;
   }
+
   public void setId(Integer id) {
     this.id = id;
   }
@@ -66,34 +67,39 @@ public class SchoolModel {
   public String getNome() {
     return nome;
   }
+
   public void setNome(String nome) {
     this.nome = nome;
   }
 
   public LocalDate getDataNascimento() {
-    return data_nascimento;
+    return dataNascimento;
   }
-  public void setDataNascimento(LocalDate data_nascimento) {
-    this.data_nascimento = data_nascimento;
+
+  public void setDataNascimento(LocalDate dataNascimento) {
+    this.dataNascimento = dataNascimento;
   }
 
   public String getNomeDaMae() {
-    return nomedamae;
+    return nomeDaMae;
   }
-  public void setNomeDaMae(String nomedamae) {
-    this.nomedamae = nomedamae;
+
+  public void setNomeDaMae(String nomeDaMae) {
+    this.nomeDaMae = nomeDaMae;
   }
 
   public String getNomeDoPai() {
-    return nomedopai;
+    return nomeDoPai;
   }
-  public void setNomeDoPai(String nomedopai) {
-    this.nomedopai = nomedopai;
+
+  public void setNomeDoPai(String nomeDoPai) {
+    this.nomeDoPai = nomeDoPai;
   }
 
   public Integer getDdd() {
     return ddd;
   }
+
   public void setDdd(Integer ddd) {
     this.ddd = ddd;
   }
@@ -101,6 +107,7 @@ public class SchoolModel {
   public Integer getNumero() {
     return numero;
   }
+
   public void setNumero(Integer numero) {
     this.numero = numero;
   }
@@ -108,6 +115,7 @@ public class SchoolModel {
   public Integer getRamal() {
     return ramal;
   }
+
   public void setRamal(Integer ramal) {
     this.ramal = ramal;
   }
@@ -115,6 +123,7 @@ public class SchoolModel {
   public String getEmail() {
     return email;
   }
+
   public void setEmail(String email) {
     this.email = email;
   }
@@ -122,6 +131,7 @@ public class SchoolModel {
   public String getSerie() {
     return serie;
   }
+
   public void setSerie(String serie) {
     this.serie = serie;
   }
@@ -129,6 +139,7 @@ public class SchoolModel {
   public String getTurno() {
     return turno;
   }
+
   public void setTurno(String turno) {
     this.turno = turno;
   }
@@ -136,6 +147,7 @@ public class SchoolModel {
   public String[] getAtividades() {
     return atividades;
   }
+
   public void setAtividades(String[] atividades) {
     this.atividades = atividades;
   }
--- src/main/java/projetoweb/SchoolRouters.java
+++ src/main/java/projetoweb/SchoolRouters.java
@@ -1,4 +1,5 @@
 package projetoweb;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,45 +17,40 @@ import jakarta.validation.Valid;
 
 @Controller
 public class SchoolRouters implements WebMvcConfigurer {
-  
+
   @Autowired
   SchoolRepository schoolRepository;
 
   @Override
-	public void addViewControllers(ViewControllerRegistry registry) {
+  public void addViewControllers(ViewControllerRegistry registry) {
     registry.addViewController("/").setViewName("index");
-		registry.addViewController("/response").setViewName("response");		
-	}
+    registry.addViewController("/response").setViewName("response");
+  }
 
   @GetMapping("/")
-  public String showForm(SchoolModel student) {
+  public String showForm(Model model) {
+    model.addAttribute("student", new SchoolModel());
     return "index";
-  } 
+  }
 
   @PostMapping("/")
-  public String getDataForm(@Valid SchoolModel student, BindingResult result) {
-    System.out.println(student.getNome());
-    System.out.println(student.getNomeDaMae());
-    System.out.println(student.getNomeDoPai());
-    System.out.println(student.getDataNascimento());
-    System.out.println(student.getEmail());
-    System.out.println(student.getDdd());
-    System.out.println(student.getNumero());
-    System.out.println(student.getRamal());
-    System.out.println(student.getSerie());
-    System.out.println(student.getTurno());
-    System.out.println(student.getAtividades());
-    if(result.hasErrors() && !isValidDDD(student.getDdd())) {
-      return "response";
+  public String getDataForm(@Valid @ModelAttribute("student") SchoolModel student, BindingResult result) {
+
+    if (result.hasErrors()) {
+      return "index";
+    }
+
+    if (!isValidDDD(student.getDdd())) {
+      result.rejectValue("ddd", "ddd.invalid", "DDD inválido no território nacional. Insira um DDD válido.");
+      return "index";
     }
 
     schoolRepository.save(student);
 
-    return "index";
+    return "response";
   }
 
-
-  @GetMapping(value="/students")
+  @GetMapping(value = "/students")
   public String showStudents(Model model) {
     Iterable<SchoolModel> students = schoolRepository.findAll();
     model.addAttribute("students", students);
@@ -61,7 +58,10 @@ public class SchoolRouters implements WebMvcConfigurer {
   }
 
   private boolean isValidDDD(Integer ddd) {
-    List<Integer> validDDDs = Arrays.asList(61,62,64,65,66,67,82,71, 73, 74, 75,77,85,88,98,99,83,81,87,86,89,84,79,68,96,92,97,91,93,94,69,95,63,27,28,31,32,33,34,35,37,38,21,22,24,11,12,13,14,15,16,17,18,19,41,42,43,44,45,46,51,53,54,55,47,48,49); 
+    List<Integer> validDDDs = Arrays.asList(61, 62, 64, 65, 66, 67, 82, 71, 73, 74, 75, 77, 85, 88, 98, 99, 83, 81, 87,
+        86, 89, 84, 79, 68, 96, 92, 97, 91, 93, 94, 69, 95, 63, 27, 28, 31, 32, 33, 34, 35, 37, 38, 21, 22, 24, 11, 12,
+        13, 14, 15, 16, 17, 18, 19, 41, 42, 43, 44, 45, 46, 51, 53, 54, 55, 47, 48, 49);
+
     return validDDDs.contains(ddd);
   }
 }
